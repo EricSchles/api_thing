59c5005f62e96873ff47cdfb19250083dcfb7c05
Eric Schles <ericschles@gmail.com>
2015-08-13 17:10:54 -0400

    adding

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 
 #http://blog.y3xz.com/blog/2012/08/16/flask-and-postgresql-on-heroku
 ###Models
-
+app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 db = SQLAlchemy(app)
 
